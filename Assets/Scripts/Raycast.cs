d2e797d12e66c50188645abad4c51e259a3f54f0
Seyit Ahmet Kutuzman <seyit19033247@gmail.com>
2020-10-11 22:32:23 +0300

    son
    son

--- Assets/Scripts/Raycast.cs
+++ Assets/Scripts/Raycast.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO.Pipes;
 using UnityEditorInternal;
 using UnityEngine;
 
@@ -9,16 +10,20 @@ public class Raycast : MonoBehaviour
     private float distance;
     RaycastHit hit;
     public static bool parent = true;
-    public GameObject grabber, screw;
+    public GameObject screw, screw1, screw2, screw3;
     public GameObject leg, leg1, leg2, leg3, table;
     #region Bool variables
     bool bool1 = true, bool2 = true, bool3 = true, bool4 = true, bool5 = true, bool6 = true, bool7 = true, bool8 = true,
         bool9 = true, bool10 = true, bool11 = true, bool12 = true, bool13 = true, bool14 = true, bool15 = true, bool16 = true;
-    public bool attach = false;
+    public bool attach = false, attach1 = false, attach2 = false, attach3 = false, test = false;
     #endregion
-    List<GameObject> legs = new List<GameObject>();
+    readonly List<GameObject> legs = new List<GameObject>();
     public Animator cam;
-    public Animator chivi;
+    public Animator chivi, chivi1, chivi2, chivi3;
+    public Animation screwanim, screwanim1, screwanim2, screwanim3;
+    public int a = 0;
+    public ParticleSystem screwdust, screwdust1, screwdust2, screwdust3;
+    public ParticleSystem dust, dust1, dust2, dust3;
     // Use this for initialization
     void Start()
     {
@@ -26,6 +31,14 @@ public class Raycast : MonoBehaviour
         legs.Add(leg1);
         legs.Add(leg2);
         legs.Add(leg3);
+        screwdust.Stop();
+        screwdust1.Stop();
+        screwdust2.Stop();
+        screwdust3.Stop();
+        dust.Stop();
+        dust1.Stop();
+        dust2.Stop();
+        dust3.Stop();
 
     }
     void Update()
@@ -38,6 +51,7 @@ public class Raycast : MonoBehaviour
                 leg.transform.parent = table.transform;
                 parent = false;
                 bool1 = false;
+                dust.Play();
                 break;
             }
 
@@ -47,6 +61,7 @@ public class Raycast : MonoBehaviour
                 leg.transform.parent = table.transform;
                 parent = false;
                 bool2 = false;
+                dust3.Play();
                 break;
             }
             if (bool3 && leg.transform.position.x < 1.2f && 0.5f < leg.transform.position.x && leg.transform.position.z < -5.3f && -6.1f < leg.transform.position.z)
@@ -55,6 +70,7 @@ public class Raycast : MonoBehaviour
                 leg.transform.parent = table.transform;
                 parent = false;
                 bool3 = false;
+                dust1.Play();
                 break;
             }
             if (bool4 && leg.transform.position.x > -1.2f && -0.5f > leg.transform.position.x && leg.transform.position.z < -5.3f && -6.1f < leg.transform.position.z)
@@ -63,6 +79,7 @@ public class Raycast : MonoBehaviour
                 leg.transform.parent = table.transform;
                 parent = false;
                 bool4 = false;
+                dust2.Play();
                 break;
             }
 
@@ -73,6 +90,7 @@ public class Raycast : MonoBehaviour
                 leg1.transform.parent = table.transform;
                 parent = false;
                 bool5 = false;
+                dust.Play();
                 break;
             }
 
@@ -82,6 +100,7 @@ public class Raycast : MonoBehaviour
                 leg1.transform.parent = table.transform;
                 parent = false;
                 bool6 = false;
+                dust3.Play();
                 break;
             }
             if (bool7 && leg1.transform.position.x < 1.2f && 0.5f < leg1.transform.position.x && leg1.transform.position.z < -5.3f && -6.1f < leg1.transform.position.z)
@@ -90,6 +109,7 @@ public class Raycast : MonoBehaviour
                 leg1.transform.parent = table.transform;
                 parent = false;
                 bool7 = false;
+                dust1.Play();
                 break;
             }
             if (bool8 && leg1.transform.position.x > -1.2f && -0.5f > leg1.transform.position.x && leg1.transform.position.z < -5.3f && -6.1f < leg1.transform.position.z)
@@ -98,6 +118,7 @@ public class Raycast : MonoBehaviour
                 leg1.transform.parent = table.transform;
                 parent = false;
                 bool8 = false;
+                dust2.Play();
                 break;
             }
 
@@ -108,6 +129,7 @@ public class Raycast : MonoBehaviour
                 leg2.transform.parent = table.transform;
                 parent = false;
                 bool9 = false;
+                dust.Play();
                 break;
             }
 
@@ -117,6 +139,7 @@ public class Raycast : MonoBehaviour
                 leg2.transform.parent = table.transform;
                 parent = false;
                 bool10 = false;
+                dust3.Play();
                 break;
             }
             if (bool11 && leg2.transform.position.x < 1.2f && 0.5f < leg2.transform.position.x && leg2.transform.position.z < -5.3f && -6.1f < leg2.transform.position.z)
@@ -125,6 +148,7 @@ public class Raycast : MonoBehaviour
                 leg2.transform.parent = table.transform;
                 parent = false;
                 bool11 = false;
+                dust1.Play();
                 break;
             }
             if (bool12 && leg2.transform.position.x > -1.2f && -0.5f > leg2.transform.position.x && leg2.transform.position.z < -5.3f && -6.1f < leg2.transform.position.z)
@@ -133,6 +157,7 @@ public class Raycast : MonoBehaviour
                 leg2.transform.parent = table.transform;
                 parent = false;
                 bool12 = false;
+                dust2.Play();
                 break;
             }
 
@@ -144,6 +169,7 @@ public class Raycast : MonoBehaviour
                 leg3.transform.parent = table.transform;
                 parent = false;
                 bool13 = false;
+                dust.Play();
                 break;
 
             }
@@ -154,6 +180,7 @@ public class Raycast : MonoBehaviour
                 leg3.transform.parent = table.transform;
                 parent = false;
                 bool14 = false;
+                dust3.Play();
                 break;
 
             }
@@ -163,6 +190,7 @@ public class Raycast : MonoBehaviour
                 leg3.transform.parent = table.transform;
                 parent = false;
                 bool15 = false;
+                dust1.Play();
                 break;
 
             }
@@ -172,6 +200,7 @@ public class Raycast : MonoBehaviour
                 leg3.transform.parent = table.transform;
                 parent = false;
                 bool16 = false;
+                dust2.Play();
                 break;
             }
             parent = false;
@@ -189,12 +218,6 @@ public class Raycast : MonoBehaviour
             foreach (GameObject leg in legs)
             {
                 leg.layer = LayerMask.NameToLayer("Default");
-            }
-            if(!attach)
-            {
-                chivi.enabled = false;
-                screw.transform.position = new Vector3(0.9605f, -0.819f, -6.476f);
-
             }
         }
         if (dragging)
@@ -216,8 +239,39 @@ public class Raycast : MonoBehaviour
             }
             if(Physics.Raycast(ray, out hit) && hit.collider.gameObject.tag == "Chivi")
             {
-                chivi.enabled = true;
-                StartCoroutine(Chivile());
+                if (!attach)
+                {
+                    chivi.enabled = true;
+                    StartCoroutine(Chivile());
+                }
+                if(a == 1)
+                {
+                    chivi1.enabled = true;
+                    if (!screwanim.isPlaying && test)
+                    {
+                        StartCoroutine(Chivile1());
+                    }
+                }
+                if(a == 2)
+                {
+                    chivi2.enabled = true;
+                    if (!screwanim1.isPlaying)
+                    {
+                        StartCoroutine(Chivile2());
+                    }
+                }
+                if (a == 3)
+                {
+                    chivi3.enabled = true;
+                    if (!screwanim2.isPlaying)
+                    {
+                        StartCoroutine(Chivile3());
+                    }
+                }
+                if(chivi1.enabled)
+                {
+                    test = true;
+                }
             }
         }
         if (table.transform.childCount >= 4)
@@ -232,8 +286,36 @@ public class Raycast : MonoBehaviour
     IEnumerator Chivile()
     {
         chivi.Play("Chivi");
+        screwdust.Play();
         yield return new WaitForSeconds(0.75f);
         attach = true;
+        a = 1;
         cam.SetTrigger("newCam");
+    }
+
+    IEnumerator Chivile1()
+    {
+        chivi1.Play("Chivi1");
+        screwdust1.Play();
+        yield return new WaitForSeconds(0.75f);
+        attach1 = true;
+        a = 2;
+        cam.SetTrigger("newCam1");
+    }
+    IEnumerator Chivile2()
+    {
+        chivi2.Play("Chivi2");
+        screwdust2.Play();
+        yield return new WaitForSeconds(0.75f);
+        attach2 = true;
+        a = 3;
+        cam.SetTrigger("newCam2");
+    }
+    IEnumerator Chivile3()
+    {
+        chivi3.Play("Chivi3");
+        screwdust3.Play();
+        yield return new WaitForSeconds(0.75f);
+        attach3 = true;
     }
 }
